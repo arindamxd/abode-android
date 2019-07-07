63d9e4d8a47c49595180c4818c3d52f1e83f5ae8
Arindam Karmkar <arindamxd@gmail.com>
2019-07-07 20:46:26 +0530

    initial commit

---
+++ app/src/main/java/com/arindam/abode/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arindam.abode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
